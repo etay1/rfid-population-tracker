80a5d35e84a2fda354933d859871f32e718570a0
Milau02 <michaellaufer2@gmail.com>
2023-07-06 15:44:54 -0400

    transitioned to using docker

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,url_for
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def serve_sensor():
     return render_template("sensor.html")
 
 if __name__ == '__main__':
-    app.run(port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
